24f93b2c6ea7622872388554aa71a70a052a4563
Jongho Kim <scurry2119@gmail.com>
2022-11-30 07:19:26 +0900

    Correct build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,24 @@
 plugins {
-    "java-library"
+    java
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
 }
 
 ant.importBuild("nutch-1.19/build.xml") { oldTargetName ->
-    if (oldTargetName == "build") "ant_build"
-    if (oldTargetName == "init") "ant_init"
-    else oldTargetName
+    when (oldTargetName) {
+        "build" -> "ant_build"
+        "init" -> "ant_init"
+        "jar" -> "ant_jar"
+        "test" -> "ant_test"
+        "javadoc" -> "ant_javadoc"
+        "clean" -> "ant_clean"
+        else -> oldTargetName
+    }
 }
 
+dependencies {
+    implementation("org.hibernate:hibernate-entitymanager:5.5.7.Final")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven(url = "https://repo.spring.io/snapshot")
+        maven(url = "https://repo.spring.io/milestone")
+    }
+}
+
+rootProject.name = "nutch-1.19-migration"
