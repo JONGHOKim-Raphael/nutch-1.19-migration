e35149e477c936b43d5ac34d6718a6d808e10eae
Jongho Kim <scurry2119@gmail.com>
2022-11-30 07:42:56 +0900

    Add dependencies, especially local JAR dependency: Tibero7

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,11 @@ ant.importBuild("nutch-1.19/build.xml") { oldTargetName ->
 }
 
 dependencies {
+
+    implementation("com.zaxxer:HikariCP:4.0.3")   // For Java 8
+    //implementation("com.zaxxer:HikariCP:5.0.1")   // For Java 11+
     implementation("org.hibernate:hibernate-entitymanager:5.5.7.Final")
+    implementation("org.springframework.data:spring-data-jpa:2.3.9.RELEASE")
+    val tbHome = System.getenv("TB_HOME")   // Read environment variable $TB_HOME from shell
+    runtimeOnly(files("$tbHome/client/lib/jar/tibero7-jdbc.jar"))
 }
